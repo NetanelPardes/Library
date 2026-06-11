e289d7278499280ec7f748f667efcd6ac0626c87
Netanel Pardes <sendi8475@gmail.com>
2026-06-11 15:26:13 +0300

    Defining all functions in member_db

--- database/member_db.py
+++ database/member_db.py
@@ -6,3 +6,31 @@ class Member:
         self.email = email
         self.is_active = True
         self.total_borrows = 0
+
+    def create_member(self,data):
+        pass
+
+    def get_all_members(self):
+        pass
+    
+    def get_member_by_id(self,id):
+        pass
+    
+    def update_member(self,id, data):
+        pass
+    
+    def deactivate_member(self,id):
+        pass
+    
+    def activate_member(self,id):
+        pass
+    
+    def increment_borrows(self,id):
+        pass
+      
+    def count_active_members(self):
+        pass
+    
+    def get_top_member(self):
+        pass
+    
