16015b8b64cf346acc2301f539a2658ed64194f3
Netanel Pardes <sendi8475@gmail.com>
2026-06-14 11:28:16 +0300

    add get al books function

--- database/book_db.py
+++ database/book_db.py
@@ -1,23 +1,44 @@
 import mysql.connector
+from fastapi import HTTPException
 from database.db_connection import get_connection
 
 optional_genre =  ['Fiction' , 'Non-Fiction' , 'Science' , 'History' , 'Other']
 
 class Book:
     def __init__(self):
-        self.connection = get_connection()
+        pass
     
     def create_book(self,data):
         if data['genre'] in optional_genre:
-            cursor = self.connection.cursor()
+            conn = get_connection()
+            cursor = conn.cursor()
+
             cursor.execute("INSERT INTO books (title , author , genre , is_available) VALUES (%s, %s, %s, %s)" ,(data['title'],data['author'], data['genre'],True))
-            self.connection.commit()
+            
+            conn.commit()
+            
+            new_id = cursor.lastrowid
+            
             cursor.close()
-            self.connection.close()
+            conn.close()
+            
+            return new_id
+        
+        raise HTTPException(status_code=404, detail="The book definition is incorrect.")
             
 
     def get_all_books(self):
-        pass
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute("SELECT * FROM books")
+
+        all_books = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return all_books
 
     def get_book_by_id(self,id):
         pass
--- routes/book_routes.py
+++ routes/book_routes.py
@@ -7,4 +7,8 @@ my_book = Book()
 
 @router.post("/books")
 def new_book(new_book:dict):
-    return {"book created" :my_book.create_book(new_book)}
+    return {"book created with id" :my_book.create_book(new_book)}
+
+@router.get("/books")
+def show_all_books():
+    return {"books" : my_book.get_all_books()}
